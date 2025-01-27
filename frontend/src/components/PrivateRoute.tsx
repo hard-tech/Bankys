1d8bfa43f3a70650bd3b633659e4e0febb9acfa5
Ahmad Jaber <ahmad.jaber010@yahoo.com>
2025-01-27 10:47:46 +0100

    feat: ajout d'une barre de navigation dans le composant PrivateRoute

--- frontend/src/components/PrivateRoute.tsx
+++ frontend/src/components/PrivateRoute.tsx
@@ -1,5 +1,5 @@
 // src/components/PrivateRoute.tsx
-import { Navigate, Outlet } from 'react-router-dom';
+import { Link, Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
 
 const PrivateRoute = () => {
@@ -9,7 +9,18 @@ const PrivateRoute = () => {
     return <Navigate to="/login" replace />;
   }
 
-  return <Outlet />;
+  return (
+    <div>
+        <nav>
+            {/* Navigation bar */}
+            <Link to="/">Home</Link>
+            <Link to="/profile">Profile</Link>
+            <Link to="/logout">Logout</Link>
+        </nav>
+
+        <Outlet />
+    </div>
+  );
 };
 
 export default PrivateRoute;
